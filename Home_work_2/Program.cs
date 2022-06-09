253defd825a0bcfa9cd936c00b42ef6ae298a5fb
Ibaragim Darslanov <darslanov2020@inbox.ru>
2022-06-09 03:25:31 +0300

    Add Home work 2

---
+++ Home_work_2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a<b)
+{
+    if(b<c)
+    {
+        Console.WriteLine("третье большее число");
+    }
+    else
+    {
+        Console.WriteLine("второе большее число");
+    }
+}
+else
+{
+    if(a<c)
+    {
+        Console.WriteLine("третье большее число");
+    }
+    else
+    {
+        Console.WriteLine("первое большее число");
+    }
+}
